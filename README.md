ac1372ebcd190c4e42f5d29134ae999b10d1b1dc
Kunal Soni <kunalsoni9200@gmail.com>
2023-06-02 10:56:35 +0530

    Added the interpolate_store_status_records()

--- README.md
+++ README.md
@@ -14,3 +14,23 @@ To handle the timezone data for the stores and ensure that the data sources can 
 6.	When parsing the timezone data from the CSV file, check if the timezone_str value is missing. If it is missing, use the default value of "America/Chicag
 
 7.	Save the changes to the database and close the connection
+
+The `Report` class is a SQLAlchemy model representing a table named `'reports'` in a database. It contains several columns: `id`, `report_id`, `status`, `data`, `created_at`, and `completed_at`.
+
+•	`id` is an auto-incrementing integer column and serves as the primary key for the table.
+•	`report_id` is a string column of length 255 and is used to uniquely identify each report. It is marked as `nullable=False` and `unique=True`, meaning it must have a value and should be unique among all reports.
+•	`status` is a string column of length 50 and represents the status of the report. It is marked as `nullable=False`, meaning it must have a value.
+•	`data` is a text column that stores the report data. It is marked as `nullable=False`, meaning it must have a value.
+•	`created_at` is a DateTime column that represents the timestamp when the report was created. It has a default value of the current UTC timestamp when a new report is inserted.
+•	`completed_at` is a DateTime column that represents the timestamp when the report was completed. It is initially `NULL` but gets updated when the report generation process is finished.
+
+The `Report` class also contains a `to_dict()` method, which returns a dictionary representation of the report object. The dictionary includes the `id`, `report_id`, `status`, `created_at`, and `completed_at` values in ISO 8601 format. The `completed_at` value is converted to a string if it exists, otherwise it is set to `None`.
+
+The `generate_report()` method is the main functionality of the `Report` class. It performs the following steps:
+1. Retrieves the maximum timestamp from the `StoreStatus` table using a database query.
+2. Determines the current timestamp by getting the current UTC time if there is no maximum timestamp, otherwise uses the maximum timestamp.
+3. Fetches the business hours for each store from the `StoreHours` table using a database query.
+4. Calculates the uptime and downtime for each store based on the store's business hours and the store status records within the last week.
+5. Stores the calculated report data in the `report_data` list.
+6. Updates the `data` column of the report object with the `report_data`, sets the `status` to `'Complete'`, and sets the `completed_at` timestamp to the current UTC time.
+7. Commits the changes to the database.
--- app/reports.py
+++ app/reports.py
@@ -86,3 +86,69 @@ class Report(Base):
         session.commit()
 
 
+def interpolate_store_status_records(records, start_time_local, end_time_local, current_timestamp):
+    interpolated_records = []
+    interval = end_time_local - start_time_local
+
+    # Check if there are any records available for interpolation
+    if records:
+        for i in range(interval.days + 1):
+            # Get the start and end time for the current day
+            current_day_start_time = start_time_local + timedelta(days=i)
+            current_day_end_time = current_day_start_time + timedelta(days=1)
+
+            # Filter records within the current day
+            current_day_records = [record for record in records if
+                                   current_day_start_time <= record.timestamp_utc < current_day_end_time]
+
+            # If no records are available for the current day, assume the store was inactive for the entire day
+            if not current_day_records:
+                interpolated_records.append({
+                    'timestamp_utc': current_day_start_time,
+                    'status': 'inactive'
+                })
+            else:
+                # Interpolate the status for the missing timestamps within the current day
+                previous_record = None
+                for j in range(len(current_day_records)):
+                    record = current_day_records[j]
+
+                    # Add the current record to the interpolated records
+                    interpolated_records.append({
+                        'timestamp_utc': record.timestamp_utc,
+                        'status': record.status
+                    })
+
+                    # Interpolate the status for the missing timestamps between the current and previous record
+                    if previous_record:
+                        time_diff = record.timestamp_utc - previous_record.timestamp_utc
+                        num_intervals = int(time_diff.total_seconds() / 3600)
+
+                        for k in range(1, num_intervals):
+                            interpolated_timestamp = previous_record.timestamp_utc + timedelta(hours=k)
+                            interpolated_records.append({
+                                'timestamp_utc': interpolated_timestamp,
+                                'status': interpolate_status(previous_record.status, record.status, k, num_intervals)
+                            })
+
+                    previous_record = record
+
+    # If no records are available, assume the store was inactive for the entire time interval
+    else:
+        for i in range(interval.days + 1):
+            current_day_start_time = start_time_local + timedelta(days=i)
+            interpolated_records.append({
+                'timestamp_utc': current_day_start_time,
+                'status': 'inactive'
+            })
+
+    # Sort the interpolated records based on timestamp
+    interpolated_records.sort(key=lambda x: x['timestamp_utc'])
+
+    # Add the current timestamp as the last record for extrapolation
+    interpolated_records.append({
+        'timestamp_utc': current_timestamp,
+        'status': interpolated_records[-1]['status']
+    })
+
+    return interpolated_records
